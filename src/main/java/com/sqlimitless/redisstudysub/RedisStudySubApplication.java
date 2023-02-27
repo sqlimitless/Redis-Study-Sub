53b6d81202a82789765ed9879bb0d15bd2f4f244
LeeHJ <lhjhoon@gmail.com>
2023-02-27 16:13:46 +0900

    Initial commit

---
+++ src/main/java/com/sqlimitless/redisstudysub/RedisStudySubApplication.java
@@ -0,0 +1,13 @@
+package com.sqlimitless.redisstudysub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStudySubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStudySubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sqlimitless/redisstudysub/RedisStudySubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlimitless.redisstudysub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStudySubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
